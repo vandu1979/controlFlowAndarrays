d80ffd3d047db521a94abf707939c927c3d8b16a
vandu1979 <vandana_kothari@yahoo.com>
2022-10-12 16:03:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+let letterGrade = 75
+function test(letterGrade) {
+    if (letterGrade > 90)  { console.log("A");
+    
+} 
+// else if (letterGrade < 90) { console.log("B");}
+
+ else if (letterGrade > 80) { console.log("B");
+    
+}else if (letterGrade < 80) { console.log("C");}
+
+   else  if (letterGrade >70) { console.log("C");}
+      else if (letterGrade >= 55) { console.log("D")} 
+
+      
+
+
+} 
+test(75)    
+// else {
+    
+// }
+// else {
+    
+// } }
+    
