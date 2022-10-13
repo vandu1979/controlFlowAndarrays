fc032d426290625117feb2261507fd24f426c930
vandu1979 <vandana_kothari@yahoo.com>
2022-10-12 20:01:14 -0600

    Day 2 better version

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 console.log("hello world");
-let letterGrade = 75
+let letterGrade = 85
 function test(letterGrade) {
-    if (letterGrade > 90)  { console.log("A");
-    
+       if (letterGrade >= 90) { console.log("A");    
+} else if (letterGrade >= 80) { console.log("B");  
+} else if (letterGrade >= 70) { console.log("C");
+} else if (letterGrade >= 55) { console.log("D");
+} else { console.log("F");
+}
 } 
-// else if (letterGrade < 90) { console.log("B");}
-
- else if (letterGrade > 80) { console.log("B");
-    
-}else if (letterGrade < 80) { console.log("C");}
-
-   else  if (letterGrade >70) { console.log("C");}
-      else if (letterGrade >= 55) { console.log("D")} 
-
-      
-
-
-} 
-test(75)    
+test(90)    
 // else {
     
 // }
