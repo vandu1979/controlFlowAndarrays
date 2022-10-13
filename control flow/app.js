ad37b80e65c7b966a897df5e914d9c6d46214c35
vandu1979 <vandana_kothari@yahoo.com>
2022-10-13 16:30:54 -0600

    First commit

---
+++ control flow/app.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+var num = 9;
+if (num > 0) {console.log("positive");
+} else if (num < 0) {console.log("negative");
+} else {console.log("zero");}
+var age = 20;
+if (age >= 18) {
+    console.log("access granted");
+} else {
+    console.log("access denied");
+}
+var num 
+       if (num > 100) {console.log("positive & greater than");
+} else if (num < 100) {console.log("positive");
+       if (num < 100) {console.log("positive but less than 100");
+} else if (num < 0) {console.log("negative");
+} else {console.log("put any number");
+}
+
+
+} 
+let letterGrad = 90
+
+function test(letterGrade) {
+       if (letterGrade >= 90) { console.log("A");    
+} else if (letterGrade >= 80) { console.log("B");  
+} else if (letterGrade >= 70) { console.log("C");
+} else if (letterGrade >= 55) { console.log("D");
+} else { console.log("F");
+}
+} 
+test(90)    
+// else {
+    
+// }
+// else {
+    
+// } }
+    
---
+++ loops/loops.js
@@ -0,0 +1,46 @@
+ // for loop
+ 
+ for (let i=0;i<10;i=i+1){
+    console.log(i);
+ }
+
+ for (let i=1;i<9;i=i+2)
+ {console.log(i);
+}
+
+
+
+ 
+ // creat an array of favmovies
+
+const favmovies = ["DDLJ", "HAHK", "TITANIC"];
+let firstmovie = favmovies[0];
+console.log(firstmovie);
+
+// push
+
+favmovies.push("TITANIC");
+
+console.log(favmovies);
+
+//pop
+
+favmovies.pop()
+
+console.log(favmovies);
+
+//unshift 
+
+favmovies.unshift("HDCS")
+favmovies.unshift("DIL")
+console.log(favmovies);
+
+favmovies.shift();
+console.log(favmovies);
+
+fruits.forEach(function(fruit) { console.log(fruit);
+} );
+
+fruits.forEach(function(fruit,idx) {console.log(idx +')' +fruit);
+});
+
