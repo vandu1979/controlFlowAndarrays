a751de782a84daca0e86d263963ff34e5f6ce41c
vandu1979 <vandana_kothari@yahoo.com>
2022-10-18 13:46:48 -0600

    my name

--- control flow/functions/functions.js
+++ control flow/functions/functions.js
@@ -1,1 +1,60 @@
 console.log("funtionslesson");
+function areBothEven(n1,n2){
+    console.log(n1 % 2)
+    console.log(0)
+     return !(n1 % 2) && !(n1 % 2);
+
+}
+console.log(areBothEven(3, 7));
+
+//name: "Maria",
+//skills: ["html", "css", ]
+let maria = getDevObject("Maria" , "HTML", "CSS", "JavaScript");
+
+function getDevObject(name){
+    let skills = [];
+function getDevObject(name, ....skills){}
+//for(let i = 1; i < arguments.length; i++){
+ //   skills.push(argument[i]);
+//}  
+//return { devName : name,
+          jobskills : skills
+//        }
+
+//}  
+//console.log(getDevObject(maria));   
+
+/*const sayName = (name) => {
+   // console.log('Hello my name is ' + name);// concatination
+   console.log(`Hello my name is ${name}`;)
+
+}
+sayName("Kadijs");
+sayName("Max the Maverick");
+sayName("Overton");*/
+
+/*const calculateAreaOgSquare  = (length, height) => {
+    let sqft = length * height;
+    console.log(`My square or rectangle is ${}`)
+
+}
+    calculateAreaOgSquare(38, 18);*/
+
+    const calculateAreaOfTriangle = (base, height) => {
+        let sqft = 1/2 (base * height);
+        console.log(`The area Of Triangle is ${}`)
+    }
+
+
+
+
+
+const circle = radius => () => {
+     let  radius = radius(" What is the radius of you circle? \t");
+     let circleArea = math.3.14 * radius * radius;
+    
+    console.log("The area of circle is" +  circleArea)
+}
+
+calculateAreaOfCircle();
+    
